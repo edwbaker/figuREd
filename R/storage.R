014a8011d67d69846bf7624df9cdabe7a37a1066
Ed Baker <929684+edwbaker@users.noreply.github.com>
2022-10-24 16:43:58 +0100

    Initial commit.

---
+++ R/storage.R
@@ -0,0 +1,30 @@
+#' Storage costs
+#'
+#' Graph of storage costs over time
+#' @importFrom rvest read_html html_nodes html_table
+#' @importFrom graphics axis
+#'
+#' @export
+
+storageCosts <- function() {
+
+  drive_url <- "https://jcmit.net/diskprice.htm"
+
+  page <- read_html(drive_url)
+
+  drives <- page |> html_nodes("table") |> html_table(fill = TRUE)
+  drives <- drives[[1]]
+
+  #Fix naming
+  cn <- drives[1,]
+  cn[1] <- "date"
+  cn[2] <- "US$/MB"
+  drives <- drives[3:nrow(drives),]
+  colnames(drives) <- cn
+
+  #allow for empty US$/MB
+  drives <- drives[which(drives$`US$/MB`!=""),]
+
+  plot(drives$date, drives$`US$/MB`, log="y", cex=.3, yaxt = "n", xlab="Year", ylab="US$ / MB")
+  axis(2, at=c(0.01,0.1,1,10,100), labels = c("0.01","0.1","1","10","100"))
+}
