4dac1355387b323a322bdc92944ab12658814383
Ed Baker <929684+edwbaker@users.noreply.github.com>
2022-10-25 18:03:24 +0100

    waveSampled

--- R/waveSampling.R
+++ R/waveSampling.R
@@ -14,3 +14,19 @@ waveSampling <- function(wave, plot_every){
   abline(h=0)
   segments(x,0,x,y)
 }
+
+#' Show a sampled wave
+#'
+#' @param wave A Wave object
+#' @param plot_every Sample rate to show
+#' @importFrom graphics barplot
+#' @export
+waveSampled <- function(wave, plot_every){
+  n <- floor(length(wave@left)/plot_every)
+
+  x <- seq(from=plot_every, by=plot_every, length.out=n)
+  y <- wave@left[x]
+
+  barplot(y, xlab="Time", xaxt="n", ylab="Amplitude", ylim=c(-1,1))
+  abline(h=0)
+}
