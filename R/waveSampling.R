46f8055ff29195a0f79a785065aee80c0422e466
Ed Baker <929684+edwbaker@users.noreply.github.com>
2022-10-26 15:52:58 +0100

    waveSampled plot waveform.

--- R/waveSampling.R
+++ R/waveSampling.R
@@ -20,19 +20,30 @@ waveSampling <- function(wave, plot_every){
 #' @param wave A Wave object
 #' @param plot_every Sample rate to show
 #' @param sample.depth Number of sampling levels to use
-#' @importFrom graphics barplot
+#' @param show_wave Logical. Whether to plot the waveform.
+#' @importFrom graphics barplot lines
 #' @export
-waveSampled <- function(wave, plot_every, sample.depth){
+waveSampled <- function(wave, plot_every, sample.depth, show_wave=T){
   sample.depth <- sample.depth/2
   n <- floor(length(wave@left)/plot_every)
 
   x <- seq(from=plot_every, by=plot_every, length.out=n)
   y <- wave@left[x]
 
-  adc <- (-sample.depth:(sample.depth -1))/sample.depth
+  adc <- (-sample.depth:sample.depth)/sample.depth
 
   y <- adc[findInterval(y, adc)]
 
-  barplot(y, xlab="Time", xaxt="n", ylab="Amplitude", ylim=c(-1,1))
+  barplot(
+    y,
+    xlab="Time",
+    xaxt="n",
+    width=plot_every,
+    space=0,
+    ylab="Amplitude",
+    ylim=c(-1,1))
+  if (show_wave) {
+    lines(1:length(wave@left), wave@left)
+  }
   abline(h=0)
 }
